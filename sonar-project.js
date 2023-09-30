f407178b3da8d289cc858c0bb547b6d4931c8cda
AlphaMale01 <78754466+AlphaMale01@users.noreply.github.com>
2023-09-29 23:49:07 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://35.183.23.83:9000/',
+  serverUrl: 'http://3.90.84.72:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': '440b9344b4051b94093e105723662a210c42c5ff',
+	    'sonar.login': '88a78219d4e0eb17bde4b8e75d887f53ded3b6db',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
